c677dc548d0bfbc222d2d7209eac1c5144b2408e
vinh tran <vinhjs@gmail.com>
2020-04-07 17:54:23 +0700

    get daily repport

--- scripts/get_daily_report.js
+++ scripts/get_daily_report.js
@@ -1,12 +1,13 @@
 const config = require("config");
 const superagent = require("superagent");
+const influx = require("influx");
+const _ = require("lodash");
 
-console.log(config.get("influxdb"));
 
 const TOKEN = config.get("slack.token");
 const CHANNEL_ID = config.get("slack.cid");
 const THREAD_ID = config.get("slack.ts");
-
+const members = config.get("members");
 const influxdb = new influx.InfluxDB({
     host: config.get("influxdb.host"),
     port: config.get("influxdb.port"),
@@ -16,17 +17,16 @@ const influxdb = new influx.InfluxDB({
         {
             measurement: 'DailyReport',
             fields: {
-                issuekey: influx.FieldType.STRING,
+                type: influx.FieldType.STRING,
                 status: influx.FieldType.INTEGER,
-                description: influx.FieldType.STRING
+                description: influx.FieldType.STRING,
+                key: influx.FieldType.STRING,
+                url: influx.FieldType.STRING
             },
             tags: [
                 'username',
                 'name',
-                'team',
-                'project',
-                'issuetype',
-                'priority'
+                'team'
             ],
             timestamp: influx.FieldType.Date
         }
@@ -36,9 +36,103 @@ const influxdb = new influx.InfluxDB({
 function getDailyReport(TOKEN, CHANNEL_ID, THREAD_ID) {
     superagent
   .get('https://slack.com/api/conversations.replies')
-  .query({ token: TOKEN, channel: CHANNEL_ID, ts: THREAD_ID }) // query string
+  .query({ token: TOKEN, channel: CHANNEL_ID, ts: THREAD_ID, limit: 50 }) // query string
   .set('accept', 'json')
   .end((err, res) => {
-    // Calling the end function will send the request
+    const body = _.get(res, 'body', null);
+    if (body && body.ok) {
+        parseResponse(body);
+    } else {
+        console.log("NO RESPONSE");
+    }
   });
 }
+function parseResponse(body){
+    const messages = body.messages || [];
+    let series = [];
+    messages.forEach(function(msg) {
+        let member = _.find(members, function(o) { return o.slackId === msg.user; });
+        if (member) {
+            let elements = _.get(msg, 'blocks[0].elements[0].elements', []);
+            let timestamp = _.get(msg, 'ts', '');
+            timestamp = parseInt(timestamp.split(".")[0]+"000");
+            series = _.union(series, getItem(timestamp, member, elements));
+        }
+    })
+    influxdb.writeMeasurement('DailyReport', series, { database: 'jira', precision: 'ms' }).then(result => {
+        console.log("result :", result);
+    }).catch(error => {
+        console.error("Error :", error, "Stack:", error.stack)
+    });
+}
+function getItem(timestamp, member, elements) {
+    console.log("getItem", member)
+    var rs = [];
+    var temp = {
+        measurement: 'DailyReport',
+        fields: {
+            type: '',
+            status: 'InProgress',
+            description: '',
+            key: '',
+            url: ''
+        },
+        tags: {
+            username: member.username,
+            name: member.name,
+            team: member.team
+        },
+        timestamp: timestamp
+    }
+var preview = '';
+    while(elements.length) {
+        item = elements.shift();
+        if (temp.fields.description && item.type === 'emoji' && (item.name === "rose" || item.name === "rocket" || item.name === "cactus")) {
+            rs.push(temp);
+            temp = temp = {
+                measurement: 'DailyReport',
+                fields: {
+                    type: '',
+                    status: 'InProgress',
+                    description: '',
+                    key: '',
+                    url: ''
+                },
+                tags: {
+                    username: member.username,
+                    name: member.name,
+                    team: member.team
+                },
+                timestamp: timestamp
+            }
+        }
+        if (item.type === 'emoji' && item.name === "rose") {
+            temp.fields.type = "Yesterday";
+        }
+        if (item.type === 'emoji' && item.name === "rocket") {
+            temp.fields.type = "Today";
+        }
+        if (item.type === 'emoji' && item.name === "cactus") {
+            temp.fields.type = "Blocking";
+        }
+        if (item.type === 'link') {
+            temp.fields.key = item.text;
+            temp.fields.url = item.url
+        }
+        if (item.type === "text") {
+            item.text = _.trim(item.text);
+        }
+        if (item.type === "text" && item.text) {
+            temp.fields.description = item.text;
+        }
+        if (item.type === 'emoji' && (item.name === "done" || item.name === "white_check_mark" || item.name === "heavy_check_mark")) {
+            temp.fields.status = "Done";
+        }
+        preview = item;
+    }
+    if (temp.fields.description) {
+        rs.push(temp);
+    }
+    return rs;
+}
+getDailyReport(TOKEN, CHANNEL_ID, THREAD_ID);
